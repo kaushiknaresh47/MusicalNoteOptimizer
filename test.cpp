ba476156defd5c0f8256b0883155e1535a9113a6
Kaushik Naresh <bkaushik6@gmail.com>
2020-09-17 15:35:51 +0530

    added test & changed helper

---
+++ test.cpp
@@ -0,0 +1,9 @@
+//
+//  test.cpp
+//  MusicalNoteOptimizer
+//
+//  Created by Kaushik Naresh on 9/17/20.
+//  Copyright © 2020 Kaushik Naresh. All rights reserved.
+//
+
+#include "test.hpp"
---
+++ test.h
@@ -0,0 +1,14 @@
+//
+//  test.hpp
+//  MusicalNoteOptimizer
+//
+//  Created by Kaushik Naresh on 9/17/20.
+//  Copyright © 2020 Kaushik Naresh. All rights reserved.
+//
+
+#ifndef test_hpp
+#define test_hpp
+
+#include <stdio.h>
+
+#endif /* test_hpp */
